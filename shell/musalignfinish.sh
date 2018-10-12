3a4e5b9daa5bccc377e52eafe128d7fa39899198
Mike Leukam <mleukam1@gmail.com>
2018-10-11 23:39:45 -0500

    working mouse alignment

--- shell/musalignfinish.sh
+++ shell/musalignfinish.sh
@@ -43,20 +43,33 @@ TMP_DIR=/scratch/mleukam/temp
 bwa aln -t 12 GRCm38p6_ref.fa A20_temp.fq > A20_temp.sai
 bwa samse -t 12 GRCm38p6_ref.fa A20_temp.sai A20_temp.fq > A20_temp.sam
 #
+#### FILTER, SORT AND MERGE ####
+#
 # filter out unmapped and multimapped reads
-samtools view -F 4 -q 1 A20_temp.sam > A20_temp.sam
+# these reads are not useful for variant calling and create downstream errors
+samtools view -F 4 -q 1 A20_temp.sam > A20_aligned.filtered.sam
+#
+# merge in picard sequence dictionary created in setup
+# create a new file (unsortedtemp.sam) that has both the dictionary and the aligned reads.
+# using this template: cat dictionary.sam > unsorted_file.sam && cat file.sam >> unsorted_file.sam
+cat GRCm38p6_ref.dict > A20_dict.sam && cat A20_aligned.filtered.sam >> A20_dict.sam
 #
 # samtools sort creates downstream errors with picard tools
 # only recommend sorting using picard tools
 java -Xmx16G -jar ${PICARD} SortSam \
-      I=A20_temp.sam \
-      O=A20_aligned.bam \
-      SORT_ORDER=coordinate
+I=A20_dict.sam \
+O=A20_aligned.query.bam \
+SORT_ORDER=queryname
 #
-# merge aligned bam with ubam to preserve read information
+java -Xmx16G -jar ${PICARD} SortSam \
+I=A20_unaligned.bam \
+O=A20_unaligned.query.bam \
+SORT_ORDER=queryname
+#
+# merge aligned bam with ubam to restore headers, quality and read group information
 java -Xmx16G -jar ${PICARD} MergeBamAlignment \
-ALIGNED_BAM=A20_aligned.bam \
-UNMAPPED_BAM=A20_unaligned.bam \
+ALIGNED_BAM=A20_aligned.query.bam \
+UNMAPPED_BAM=A20_unaligned.query.bam \
 OUTPUT=A20_merged.bam \
 R=/scratch/mleukam/mouse/GRCm38p6_ref.fa \
 CREATE_INDEX=true ADD_MATE_CIGAR=true \
@@ -65,8 +78,22 @@ INCLUDE_SECONDARY_ALIGNMENTS=true MAX_INSERTIONS_OR_DELETIONS=-1 \
 PRIMARY_ALIGNMENT_STRATEGY=MostDistant ATTRIBUTES_TO_RETAIN=XS \
 TMP_DIR=/scratch/mleukam/temp
 #
-# clean up and remove temporary files
+# sort merged BAM by coordinate for later analysis
+java -Xmx16G -jar ${PICARD} SortSam \
+I=A20_merged.sam \
+O=A20_merged.sorted.bam \
+SORT_ORDER=coordinate
+#
+#### CLEAN UP ####
+#
+# remove temporary files
 rm A20_temp.fq
 rm A20_temp.sai
 rm A20_temp.sam
 rm A20_aligned.bam
+rm A20_filteredtemp.sam
+rm A20_unsortedtemp.sam
+rm A20_faligned.filtered.sam
+rm A20_unaligned.query.bam
+rm A20_aligned.query.bam
+rm A20_dict.sam
